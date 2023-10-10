21f89674ba226b4e805cd0199165f5b30c9b3e56
PaBoba <140555335+PaBoba@users.noreply.github.com>
2023-10-10 16:45:00 -0700

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,42 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is your Title?',
+        name: 'Title',
+    },
+    {
+        type: 'input',
+        message: 'What is yor description?',
+        name: 'Description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation insturctions?',
+        name: 'Installation',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage information?',
+        name: 'Usage',
+    },
+    {
+        type: 'input',
+        message: 'What are the contirubtion guidelines?',
+        name: 'Contributing',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions?',
+        name: 'Tests',
+    },
+    {
+        type: 'list',
+        name: 'License',
+        message: 'License options',
+        choices: ["Blubasaur", "Squirtle", "Charmander"]
+    },
 ];
 
 // TODO: Create a function to write README file
